376215bb7e28ff8650b0c99aa70f4236fc6e7734
kinjo yuya <kinjo.bobchin@gmail.com>
2022-02-25 20:58:03 +0900

    chore: new Mapでtodo追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = "todo"
+  title = 'todo';
+
+  todos = new Map([
+    ['todo1', new Date()],
+    ['todo2', new Date()],
+    ['todo3', new Date()],
+  ]);
 }
