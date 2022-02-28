7ef9c20667dfbbf2672c30a0f0fc6f7760772d02
kinjo yuya <kinjo.bobchin@gmail.com>
2022-02-28 13:20:51 +0900

    feat: リロードしてもバックアップして消えないように修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,19 +17,22 @@ export class AppComponent {
     { title: '完了', status: true },
   ] as const;
 
-  todos = new Map<string, boolean>();
+  todos = new Map<string, boolean>(Object.entries(JSON.parse(localStorage.getItem('backup') ?? '{}')));
 
   add() {
     if (!this.form.value) return;
     this.todos.set(this.form.value, false);
     this.form.setValue('');
+    localStorage.setItem('backup', JSON.stringify(Object.fromEntries(this.todos)));
   }
 
   changeStatus(todo: KeyValue<string, boolean>) {
     this.todos.set(todo.key, !todo.value);
+    localStorage.setItem('backup', JSON.stringify(Object.fromEntries(this.todos)));
   }
 
   remove(title: string) {
     this.todos.delete(title);
+    localStorage.setItem('backup', JSON.stringify(Object.fromEntries(this.todos)));
   }
 }
