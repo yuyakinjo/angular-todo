4b548dd181946732a68b2eb872515b546b3f828c
kinjo yuya <kinjo.bobchin@gmail.com>
2022-03-02 16:50:38 +0900

    chore: todo-countに移植完了

--- src/app/todo-count/todo-count.component.ts
+++ src/app/todo-count/todo-count.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TodoService } from '../service/todo.service';
 
 @Component({
   selector: 'app-todo-count',
   templateUrl: './todo-count.component.html',
-  styleUrls: ['./todo-count.component.scss']
+  styleUrls: ['./todo-count.component.scss'],
 })
-export class TodoCountComponent implements OnInit {
+export class TodoCountComponent {
+  todos = this.todoService.todos;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private todoService: TodoService) {}
 }
