46cb80eac5b86e83764425c67c949bc9b7703f91
kinjo yuya <kinjo.bobchin@gmail.com>
2022-03-02 16:39:02 +0900

    new: npx ng g c todo-input --skip-tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DonePipe } from './done.pipe';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoInputComponent } from './todo-input/todo-input.component';
 
 @NgModule({
-  declarations: [AppComponent, DonePipe, TodoListComponent],
+  declarations: [AppComponent, DonePipe, TodoListComponent, TodoInputComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
