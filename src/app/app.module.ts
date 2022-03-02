c71d1541db499280e3b7c45b6bde6a3ad3a09f96
kinjo yuya <kinjo.bobchin@gmail.com>
2022-03-02 15:36:08 +0900

    new: npx ng g c todo-list --skip-tests -c OnPush

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DonePipe } from './done.pipe';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [AppComponent, DonePipe],
+  declarations: [AppComponent, DonePipe, TodoListComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
