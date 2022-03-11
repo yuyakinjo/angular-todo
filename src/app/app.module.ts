b53e38e757ac751a93d63458d8bb1b946afe7e98
kinjo yuya <kinjo.bobchin@gmail.com>
2022-03-11 16:55:59 +0900

    module追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DonePipe } from './done.pipe';
 import { TodoCountComponent } from './todo-count/todo-count.component';
 import { TodoInputComponent } from './todo-input/todo-input.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, DonePipe, TodoListComponent, TodoInputComponent, TodoCountComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
